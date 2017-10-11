6072ce48d774cae11da83642d10a947878d1f1cb
victorclee <victorchristopherlee@gmail.com>
2017-10-10 21:45:39 -0500

    Initial commit: clock

---
+++ clock.rb
@@ -0,0 +1,14 @@
+# Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+
+# Your task is to make 'Past' function which returns time converted to miliseconds.
+
+# #####Example:
+
+# past(0, 1, 1) == 61000
+# Note! h, m and s will be only Natural numbers!
+
+def past(h, m, s)
+
+end
+
+
